78f67803607de9964ead37fdeaa0325b64047f3a
Sidney Cremer <sidcremer@gmail.com>
2019-05-20 16:57:03 +1000

    Innital commit
    Initial wireframe and base project commit.

---
+++ trainbrain/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  trainbrain
+//
+//  Created by Sidney Cremer on 13/5/19.
+//  Copyright © 2019 Sidney Cremer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    
+}
+
