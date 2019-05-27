2169fd14027d1ae4cba8d276d4793ab814158301
Sidney Cremer <sidcremer@gmail.com>
2019-05-27 19:47:55 +1000

    Initial commit for Sidney Branch. Added Navigation Controller
    Added a navigation controller and back buttons for all views other than inital view. Current segue from final view does not reset any data.

--- trainbrain/ViewController.swift
+++ trainbrain/ViewController.swift
@@ -29,5 +29,7 @@ class ViewController : UIViewController {
             }
         platformViewController.station = station
     }
+    
+    @IBAction func unwind(_ sender: UIStoryboardSegue) {}
 }
 
